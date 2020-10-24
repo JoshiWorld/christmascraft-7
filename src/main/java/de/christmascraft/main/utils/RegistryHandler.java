f0c476e4a95d57cc4587de869839afe6dc85d797
JoshiWorld <joshua.stieber.1903@gmail.com>
2020-10-24 20:14:08 +0200

    Pupsen

---
+++ src/main/java/de/christmascraft/main/utils/RegistryHandler.java
@@ -0,0 +1,11 @@
+package de.christmascraft.main.utils;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class RegistryHandler {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<Item>(ForgeRegistries.ITEMS);
+
+}
