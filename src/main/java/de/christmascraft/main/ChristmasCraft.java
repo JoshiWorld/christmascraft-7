62467ad50b4fd82837ea3914fbe2c4c4fd4fa8bb
JoshiWorld <joshua.stieber.1903@gmail.com>
2020-10-24 19:25:10 +0200

    First commit

---
+++ src/main/java/de/christmascraft/main/ChristmasCraft.java
@@ -0,0 +1,29 @@
+package de.christmascraft.main;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("christmascraft")
+public class ChristmasCraft {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public ChristmasCraft() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
