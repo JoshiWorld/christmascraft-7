04b188629f364ed23d6abe786936b9f1c4b563b0
JoshiWorld <joshua.stieber.1903@gmail.com>
2020-10-25 23:17:43 +0100

    Added first item & texturefolders

---
+++ src/main/java/de/christmascraft/main/init/ModItems.java
@@ -0,0 +1,16 @@
+package de.christmascraft.main.init;
+
+import de.christmascraft.main.ChristmasCraft;
+import de.christmascraft.main.items.ItemBase;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class ModItems {
+    public static DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ChristmasCraft.MOD_ID);
+
+    // Items
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
+
+}
